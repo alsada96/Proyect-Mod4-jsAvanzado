2f75c3a3a09c8a57d448e8d4a4f7f5916fb03033
Alvaro del Angel <nachodelangel96@gmail.com>
2022-08-31 22:58:00 -0500

    feat: modficacion de parametros id por sku

--- src/controllers/books.controllers.js
+++ src/controllers/books.controllers.js
@@ -1,70 +1,108 @@
-import {getConnection} from "./../commons/connection"
+/* Importing the getConnection function from the connection.js file. */
+import { getConnection } from "./../commons/connection"
 
+/**
+ * It gets a connection to the database, then it queries the database for all the books, then it sends
+ * the result back to the client.
+ * @param req - The request object. This object represents the HTTP request and has properties for the
+ * request query string, parameters, body, HTTP headers, and so on.
+ * @param res - The response object.
+ */
 const getBooks = async (req, res) =>{
     try{
         const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM books");
-        res.json(result)
+        const result = await connection.query("SELECT * FROM books;");
+        res.json(result);
     }catch (error){
         res.status(500);
-        res.send(error.message)
+        res.send(error.message);
     }
-}
+};
 
+/**
+ * It gets a book from the database and returns it as a JSON object.
+ * @param req - The request object. This contains information about the HTTP request that raised the
+ * event.
+ * @param res - The response object.
+ */
 const getBook = async (req, res) =>{
     try{
-        const { sku } = req.params;
+        const { id } = req.params;
         const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM books WHERE sku = ?", sku);
-        res.json(result)
-    }catch (error){
+        const result = await connection.query("SELECT * FROM books WHERE id = ?;", id);
+        res.json(result);
+    }catch (error) {
         res.status(500);
-        res.send(error.message)
+        res.send(error.message);
     }
-}
+};
 
+/**
+ * It takes the data from the request body, checks if all the fields are filled, and if they are, it
+ * creates a new book object and inserts it into the database.
+ * @param req - The request object. This represents the HTTP request and has properties for the request
+ * query string, parameters, body, HTTP headers, and so on.
+ * @param res - The response object.
+ */
 const addBook = async (req, res) =>{
     try{
-        const { sku, name_product, price, description_product, url_image } = req.body;
-        if (sku === undefined || name_product === undefined || price === undefined || price === description_product || url_image === undefined ){
+        const { title, total_pages, author, nationality, language, cover_url, editorial } = req.body;
+
+        if (title === undefined || total_pages === undefined || author === undefined || nationality === undefined || language === undefined || cover_url === undefined || editorial === undefined ) {
             res.status(400).json({message:"Bad Request. Please fill all field."})
         }
+
+        const book = { title, total_pages, author, nationality, language, cover_url, editorial }
         const connection = await getConnection();
-        const result = await connection.query("INSERT INTO `products`(`sku`, `name_product`, `price`, `description_product`, `url_image`) VALUES (?,?,?,?,?)", [sku, name_product, price, description_product, url_image]);
-        res.json(result)
-    }catch (error){
+        await connection.query("INSERT INTO books SET ?", book);
+        res.json({message: "Book added"});
+    }catch (error) {
         res.status(500);
         res.send(error.message)
     }
-}
+};
 
+/**
+ * It updates a book in the database
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 const updateBook = async (req, res) =>{
     try{
-        const {sku} = req.params;
-        const { name_product, price, description_product, url_image } = req.body;
-        if (name_product === undefined || price === undefined ){
-            res.status(400).json({message:"Bad Request. Please fill all field."})
-        }
-        const connection = await getConnection();
-        const result = await connection.query("UPDATE products SET `name_product`= ?,`price`=?,`description_product`=?,`url_image`=? WHERE sku= ?", [name_product, price, description_product, url_image,sku]);
-        res.json(result)
-    }catch (error){
-        res.status(500);
-        res.send(error.message)
-    }
-}
+        const { id } = req.params;
+        const { title, total_pages, author, nationality, language, cover_url, editorial } = req.body;
 
-const deleteBook = async (req, res) =>{
-    try{
-        const {sku} = req.params;
+        if ( title === undefined || total_pages === undefined || author === undefined || nationality === undefined || language === undefined || cover_url === undefined || editorial === undefined ) {
+            res.status(400).json({message:"Bad Request. Please fill all field."});
+        }
+
+        const book = { title, total_pages, author, nationality, language, cover_url, editorial };
         const connection = await getConnection();
-        const result = await connection.query("DELETE FROM products WHERE sku= ?", sku);
+        const result = await connection.query("UPDATE books SET ? WHERE id = ?", [book, id]);
         res.json(result)
     }catch (error){
         res.status(500);
         res.send(error.message)
     }
-}
+};
+
+/**
+ * It deletes a book from the database based on the id of the book.
+ * @param req - The request object. This object represents the HTTP request and has properties for the
+ * request query string, parameters, body, HTTP headers, and so on.
+ * @param res - The response object.
+ */
+const deleteBook = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const connection = await getConnection();
+        const result = await connection.query("DELETE FROM books WHERE id = ?", id);
+        res.json(result)
+    }catch (error){
+        res.status(500);
+        res.send(error.message)
+    }
+};
 
 export const methods = {
     getBooks,
@@ -73,4 +111,4 @@ export const methods = {
     updateBook,
     deleteBook
 
-}
+};
--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -4,10 +4,10 @@ import { methods as bookController } from "./../controllers/books.controllers"
 const router = Router();
 
 /* A router that is used to create the routes for the API. */
-router.get("/", bookController.getBooks); // GET http://localhost:5000/api-restfull/products/
-router.get("/:sku", bookController.getBook); // GET http://localhost:5000/api-restfull/products/:sku
-router.post("/", bookController.addBook); // POST http://localhost:5000/api-restfull/products/
-router.put("/:sku", bookController.updateBook); // PUT http://localhost:5000/api-restfull/products/:sku
-router.delete("/:sku", bookController.deleteBook);// DELETE http://localhost:5000/api-restfull/products/:sku
+router.get("/", bookController.getBooks); 
+router.get("/:id", bookController.getBook); 
+router.post("/", bookController.addBook); 
+router.put("/:id", bookController.updateBook); 
+router.delete("/:id", bookController.deleteBook);
 
 export default router;
