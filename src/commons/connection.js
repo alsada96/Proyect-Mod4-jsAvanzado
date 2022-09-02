5d1682190fde695e75e4d7c7040c45c0cf854d2c
Alvaro del Angel <nachodelangel96@gmail.com>
2022-09-01 20:19:46 -0500

    feat:prueba para heroku archivo .env

--- src/commons/connection.js
+++ src/commons/connection.js
@@ -3,7 +3,7 @@ import mysql from "promise-mysql";
 import config  from "./../config.js";
 
 /* Creating a connection to the database. */
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: config.host,
     database: config.database,
     user: config.user,
